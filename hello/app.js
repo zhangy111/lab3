c2bcf9e3b851af70c184a21c5655aad332b38d8d
Henry Zhang <yuz087@ucsd.edu>
2020-01-23 13:41:16 -0800

    finished lab 3

--- hello/app.js
+++ hello/app.js
@@ -5,11 +5,20 @@
 
 var express = require('express');
 var http = require('http');
+//var routes = require('./routes');
+//var user = require('./routes/user');
 var path = require('path');
-var handlebars = require('express3-handlebars')
+var handlebars = require('express3-handlebars');
 
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 var hello = require('./routes/hello');
-
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
 var app = express();
 
 // all environments
@@ -17,19 +26,25 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
-app.use(app.router);
+//app.set('view engine', 'pug')
+app.use(favicon(path.join(__dirname, '/node_modules/st/favicon.ico')));
+//app.use(express.favicon());
+app.use(logger('dev'));
+//app.use(express.logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+//app.use(express.methodOverride());
+//app.use(multer());
+app.use(cookieParser('Intro HCI secret key'));
+app.use(session());
+//app.use(express.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+//  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 // Add routes here
